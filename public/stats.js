5e828976a62d329933b8d06baa8502b124074ed5
Klas <klas@blurg.se>
2024-05-06 20:19:22 +0200

    Some stats

--- public/stats.js
+++ public/stats.js
@@ -8,9 +8,18 @@ class Stats {
         try {
             const resp = await fetch('/stats');
             this.data = await resp.json();
+            console.log(this.data);
+
+            this.updateData()
         } catch (err) {
 
         }
+    }
+
+    updateData = () => {
+        document.getElementById("total-visitors").textContent = this.data.total_visitors;
+        document.getElementById("current-visitors").textContent = this.data.current_visitors;
+        document.getElementById("total-page-views").textContent = this.data.total_page_views;
     }
 }
 
